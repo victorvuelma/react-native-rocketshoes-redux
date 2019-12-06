58f04937d2311210ad6c50a1dd9e799ac54dea2c
Victor Vuelma <victorvuelma@outlook.com>
2019-12-06 00:08:45 -0300

    use react navigation with hooks

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigation } from 'react-navigation-hooks';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -12,13 +13,15 @@ import {
   ProductCount,
 } from './styles';
 
-export default function Header({ navigation }) {
+export default function Header() {
   const cartSize = useSelector(state => state.cart.length);
+
+  const navigation = useNavigation();
 
   return (
     <Wrapper>
       <Container>
-        <LogoTouch onPress={() => navigation.goBack()}>
+        <LogoTouch onPress={() => navigation.goBack(null)}>
           <Logo />
         </LogoTouch>
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
